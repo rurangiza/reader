560757413c8334c3d94f0e739ecdd65c060c96ee
Arsene <a.rurangiza@gmail.com>
2025-02-08 16:52:09 +0100

    [IMP] ui(file_upload): adds pfd upload + dyanmic ui

---
+++ app/src/components/Button.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface Props {
+  type: "button" | "submit" | "reset";
+  children: string;
+  rounded?: boolean;
+  disabled?: boolean;
+}
+
+const Button = ({
+  type = "button",
+  children,
+  rounded = false,
+  disabled,
+}: Props) => {
+  return (
+    <div>
+      <button
+        className={`max-w-full px-5 py-3 ${
+          disabled
+            ? "bg-[#DEE6FF] text-[#AEBDF2] cursor-default"
+            : "bg-blue-600 text-white cursor-pointer"
+        } ${rounded ? "rounded-sm" : ""}`}
+        type={type}
+        style={{
+          width: "100%",
+        }}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
